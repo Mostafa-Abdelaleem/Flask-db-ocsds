858bbd1f08bcf8bf31ced24ea9b08acf7033ce7d
mostafa3m <35612762+mostafa3m@users.noreply.github.com>
2020-04-26 05:47:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def main():
 
 @app.route('/db2')
 def access_db():
-    conn=ibm_db.connect(os.environ['dbcred'], "", "")
+    conn=ibm_db.connect(str(os.environ['dbcred']), "", "")
     sql = ' select * from NAMES '
     stmt = ibm_db.exec_immediate(conn,sql)
     dictionary = ibm_db.fetch_both(stmt)
@@ -23,14 +23,14 @@ def access_db():
 
 @app.route('/abdelaleem')
 def abd_db():
-    conn=ibm_db.connect(os.environ['dbcred'], "", "")
+    conn=ibm_db.connect(str(os.environ['dbcred']), "", "")
     sql = ''' UPDATE Names SET  name = 'abdelaleem' WHERE id='1'; '''
     ibm_db.exec_immediate(conn,sql)
     return "name is now abdelelaeem"
 
 @app.route('/mostafa')
 def mos_db():
-    conn=ibm_db.connect(os.environ['dbcred'], "", "")
+    conn=ibm_db.connect(str(os.environ['dbcred']), "", "")
     sql = ''' UPDATE Names SET  name = 'mostafa' WHERE id='1'; '''
     ibm_db.exec_immediate(conn,sql)
     return "name is now mostafa"
