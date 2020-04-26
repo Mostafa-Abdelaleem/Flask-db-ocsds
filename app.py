8021fca188326bdeb4b2e981f822a27bc3bb3990
mostafa3m <35612762+mostafa3m@users.noreply.github.com>
2020-04-26 05:42:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-conn=ibm_db.connect( str(os.environ['dbcred']), "", "")
+conn=ibm_db.connect(os.environ['dbcred'], "", "")
 
 
 @app.route("/")
@@ -14,6 +14,7 @@ def main():
 
 @app.route('/db2')
 def access_db():
+    conn=ibm_db.connect(os.environ['dbcred'], "", "")
     sql = ' select * from NAMES '
     stmt = ibm_db.exec_immediate(conn,sql)
     dictionary = ibm_db.fetch_both(stmt)
@@ -22,12 +23,14 @@ def access_db():
 
 @app.route('/abdelaleem')
 def abd_db():
+    conn=ibm_db.connect(os.environ['dbcred'], "", "")
     sql = ''' UPDATE Names SET  name = 'abdelaleem' WHERE id='1'; '''
     ibm_db.exec_immediate(conn,sql)
     return "name is now abdelelaeem"
 
 @app.route('/mostafa')
 def mos_db():
+    conn=ibm_db.connect(os.environ['dbcred'], "", "")
     sql = ''' UPDATE Names SET  name = 'mostafa' WHERE id='1'; '''
     ibm_db.exec_immediate(conn,sql)
     return "name is now mostafa"
