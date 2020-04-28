477f873a042dc4364f98b30eff9051158db4d36a
mostafa3m <35612762+mostafa3m@users.noreply.github.com>
2020-04-29 00:46:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,31 +7,53 @@ conn=ibm_db.connect(os.environ['dbcred'], "", "")
 
 @app.route("/")
 def main():
-    return "Flask app is online!"
+    try:
+         sql = ''' CREATE TABLE values(id int,name varchar(50)) '''
+         stmt = ibm_db.exec_immediate(conn,sql)
+         err="Table Values Created In Db2 Database"
+    except:
+        err="Table Values Already Exists"
+    return "<h1 style='color:green;text-align:center;''>Flask App Is Online!:D</br></h1>" \
+            +"<h2 style='text-align:center;''>Creating Table Values In Db2 Database ...</br></h2>" \
+            +f" <h3 style='color:blue;text-align:center;'>{err}</h3>"
 
 
 @app.route('/db2')
 def access_db():
-    
-    sql = ' select * from NAMES '
-    stmt = ibm_db.exec_immediate(conn,sql)
-    dictionary = ibm_db.fetch_both(stmt)
-    return dictionary['NAME']
+    try:
+        sql = ''' SELECT * FROM values '''
+        stmt = ibm_db.exec_immediate(conn,sql)
+        dictionary = ibm_db.fetch_both(stmt)
+        if(dictionary):
+            return "<h1 style='text-align:center;'>Table Values</br></h1>"\
+                    +f"<h2 style='color:blue;text-align:center;'></br>{dictionary['NAME']}</h2>"
+        else:
+            return "<h1 style='color:red;text-align:center;'>Table Values Is Empty"
+            
+    except:
+        return "<h1 style='color:red;text-align:center;'>Table Values Doesn't Exist"
 
 
-@app.route('/abdelaleem')
+@app.route('/insertname')
 def abd_db():
+        try:
+            sql=''' INSERT INTO values (id,name) VALUES ('1','Mostafa Abdelaleem') ; '''
+            ibm_db.exec_immediate(conn,sql) 
+            return "<h1 style='color:green;text-align:center;'> Table now has name: mostafa abdelelaeem</h1>"
+        except:
+            return "<h1 style='color:red;text-align:center;'>There Is No Table To Insert In</br>\
+                Please Create The Table And Try Again </h1>"
+        
+        
 
-    sql = ''' UPDATE Names SET  name = 'abdelaleem' WHERE id='1'; '''
-    ibm_db.exec_immediate(conn,sql)
-    return "name is now abdelelaeem"
-
-@app.route('/mostafa')
+@app.route('/deletetable')
 def mos_db():
-
-    sql = ''' UPDATE Names SET  name = 'mostafa' WHERE id='1'; '''
-    ibm_db.exec_immediate(conn,sql)
-    return "name is now mostafa"
+    try:
+        sql = ''' DROP TABLE values; '''
+        ibm_db.exec_immediate(conn,sql)
+        return "<h1 style='color:green;text-align:center;'>Table Values Is Now Deleted</h1>"
+    except:
+        return "<h1 style='color:red;text-align:center;'>Table Values Doesn't Exist Already</h1>"
 
 
 if __name__ == "__main__":
